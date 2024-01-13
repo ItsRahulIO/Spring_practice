23f156fba8c620c6d1c01eaa1414ba1f71ecaf44
Rahul <rahul.nitjsr67@gmail.com>
2024-01-13 11:20:31 +0530

    Spring test proram

---
+++ Spring_Test/src/Backcode/Spring.java
@@ -0,0 +1,18 @@
+package Backcode;
+
+
+public class Spring
+{
+    public static void main(String[] args) {
+
+        test();
+
+
+    }
+
+    public static void test()
+    {
+        System.out.println("Hello tester");
+    }
+
+}
