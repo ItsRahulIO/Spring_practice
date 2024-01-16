5f3df462da3c523402e606def1bc448fd02a6d7b
Rahul <rahul.nitjsr67@gmail.com>
2024-01-16 14:42:43 +0530

    some added new features

--- Spring_Test/src/Backcode/Spring.java
+++ Spring_Test/src/Backcode/Spring.java
@@ -5,7 +5,8 @@ public class Spring
 {
     public static void main(String[] args) {
 
-        test();
+//        test();
+        System.out.println(binarySearch(344));
 
 
     }
@@ -17,6 +18,28 @@ public class Spring
     public static void test2()
     {
         System.out.println("This is second test");
+    }
+
+    public static boolean binarySearch(int val)
+    {
+        int[] arr = new int[]{12,34,45,132,344,523,623,782,4334,5553};
+
+        int start = 0 ;
+         int end  = arr.length-1;
+
+         while (start <= end)
+         {
+             int mid   = (start + end)/2;
+
+            if (arr[mid] > val)
+                end = mid - 1;
+            else if (arr[mid] < val)
+                 start = mid +1;
+            else
+                return true;
+         }
+
+         return false;
     }
 
 }
